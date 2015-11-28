0a5744b17b01431e8cec99b442e3f329bb44b52d
RomanMokych <Roman@mordor.local>
2015-11-28 23:04:56 +0200

    Added test for archiving.

---
+++ Serializable/SerializableTests/SerializableTests.m
@@ -0,0 +1,46 @@
+//
+//  SerializableTests.m
+//  SerializableTests
+//
+//  Created by Роман on 11/28/15.
+//  Copyright © 2015 Roman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "Serializable.h"
+
+@interface SerializableA : Serializable
+
+@property (nonatomic, strong) NSString* aString;
+@property (nonatomic) BOOL aBool;
+@property (nonatomic) int aInt;
+
+@end
+
+@implementation SerializableA
+@end
+
+@interface SerializableTests : XCTestCase
+
+@end
+
+@implementation SerializableTests
+
+- (void)test_Archiving
+{
+    SerializableA* a = [[SerializableA alloc] init];
+    a.aString = @"test";
+    a.aBool = YES;
+    a.aInt = 5;
+    
+    NSData* aData = [NSKeyedArchiver archivedDataWithRootObject: a];
+    
+    SerializableA* b = [NSKeyedUnarchiver unarchiveObjectWithData: aData];
+    
+    XCTAssertEqualObjects(@"test", b.aString);
+    XCTAssertEqual(YES, b.aBool);
+    XCTAssertEqual(5, b.aInt);
+}
+
+@end
